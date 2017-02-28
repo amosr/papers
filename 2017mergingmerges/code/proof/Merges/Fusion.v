f498bc9476483ab3bc19a400f6e765bf6cf48826
Amos Robinson <amos.robinson@gmail.com>
2017-02-28 18:30:50 +1100

    proof very rough

--- 2017mergingmerges/code/proof/Merges/Fusion.v
+++ 2017mergingmerges/code/proof/Merges/Fusion.v
@@ -13,11 +13,9 @@ Section Soundness.
   Variable C : Set.
   Variable V1 : Set.
   Variable L1 : Set.
-  Variable P1 : P.Program L1 C V1.
 
   Variable V2 : Set.
   Variable L2 : Set.
-  Variable P2 : P.Program L2 C V2.
 
   Variable EqDec_C : EqDec C.
 
@@ -25,19 +23,20 @@ Section Soundness.
   Definition Heap := B.ScalarHeap (F.V C V1 V2).
   Definition InputStates := C -> F.State.
 
-  Definition fused_P1_P2 := r P1 P2 EqDec_C.
+  Definition fuse (p1 : P.Program L1 C V1) (p2 : P.Program L2 C V2) := r p1 p2 EqDec_C.
   
   Definition Var1 := @F.V'V1 C V1 V2.
   Definition Var2 := @F.V'V2 C V1 V2.
 
+
 Theorem Soundness
-    (streams : Streams) (heap    : Heap)
-    (label1  : L1)         (label2  : L2)
-    (state1  : InputStates) (state2  : InputStates)
+    (P1 : P.Program L1 C V1) (P2 : P.Program L2 C V2)
+    (streams : Streams)      (heap    : Heap)
+    (label1  : L1)           (label2  : L2)
+    (state1  : InputStates)  (state2  : InputStates)
              :
-  P.EvalBs fused_P1_P2 
-             streams heap 
-            (F.LX label1 label2 state1 state2)
+  P.EvalBs (fuse P1 P2) streams heap 
+           (F.LX label1 label2 state1 state2)
   -> F.EvalOriginal Var1 P1 P2 state1 streams heap label1
   /\ F.EvalOriginal Var2 P2 P1 state2 streams heap label2.
 Proof.
@@ -49,8 +48,8 @@ Proof.
   unfold F.EvalOriginal.
   eauto.
 
-  apply (P.InitPre fused_P1_P2).
-  apply (P.BlocksPre fused_P1_P2).
+  apply (P.InitPre (fuse P1 P2)).
+  apply (P.BlocksPre (fuse P1 P2)).
 Qed.
 
 End Soundness.
